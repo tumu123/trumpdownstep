e568bbc16a2c2aac8393ae1351f1adb9a2680bcc
tumu123 <59040051+tumu123@users.noreply.github.com>
2024-12-19 13:45:54 +0800

    Update votes.js

--- api/votes.js
+++ api/votes.js
@@ -6,10 +6,19 @@ let votes = {
 };
 
 export default function handler(req, res) {
+    // 允许跨域请求
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
     if (req.method === 'GET') {
+        // 返回当前投票数据
         res.status(200).json(votes);
-    } else if (req.method === 'POST') {
+    } 
+    else if (req.method === 'POST') {
         const { choice } = req.body;
+        
+        // 更新投票数据
         if (choice) {
             switch(choice) {
                 case 'positive':
@@ -23,6 +32,15 @@ export default function handler(req, res) {
                     break;
             }
         }
+        
+        // 返回更新后的数据
         res.status(200).json(votes);
+    }
+    else if (req.method === 'OPTIONS') {
+        // 处理预检请求
+        res.status(200).end();
+    }
+    else {
+        res.status(405).end();
     }
 }
